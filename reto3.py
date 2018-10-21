1b3073f6de776b46049aee1a76223b817e83b5f5
DavidMarLor <rr.david.marlor@gmail.com>
2018-10-21 17:15:05 +0200

    reto3.py
    Nueva version enfocandola sin recursividad, falta afianzar el codigo para corregir errores.

--- reto3.py
+++ reto3.py
@@ -5,16 +5,45 @@ num=input();
 
 num2 = 2;
 
-def primo(num, num2):
-    if num % num2 == 0:
-        return 1;
-    else:
-        num2 += 1;        
-        return 0;         
+#def primo(num, num2):
+#    if num % num2 == 0:
+#        return 1;
+#    else:
+#        num2 += 1;
+#        auxiliar = primo(num, num2);
+#        return auxiliar;
 
-comprobacion = primo(num,num2);
+comprobacion = 0;
+contador = 0;
+
+#Se debe comrpobar hasta que num y num2 sean iguales
+#Se puede intentar con un for.
+
+for x in range(0,num):
+    if num % num2 == 0:
+        print("He entrado en el if");
+        if contador != 0:
+            comprobacion = 1;
+            contador += 1;
+            print("He entrado en el if del if");
+        else:
+            comprobacion = 0;
+            print("He entrado en el else del if");
+
+    elif num == num2:
+        if contador == 0:
+            comprobacion = 0;
+        print("He entrado en el elif");
+
+    else:
+        num2 += 1;
+        print num2;
+        print("He entrado en el else");
+
+
+#comprobacion = primo(num,num2);
 
 if comprobacion == 0:
-    print("Es 1");
+    print("Es primo");
 else:
-    print("Es un 0");
+    print("No es primo");

f407327f72439df41bf8868ff4e74eb2b75dc810
DavidMarLor <rr.david.marlor@gmail.com>
2018-10-22 18:29:26 +0200

    reto3.py
    Fase 1 completada, falta pulir codigo. Actualmente indica si un numero es primo o no.
    La fase 2 consiste en mostrar todos los factoriales de los numeros no primos.

--- reto3.py
+++ reto3.py
@@ -5,43 +5,40 @@ num=input();
 
 num2 = 2;
 
-#def primo(num, num2):
-#    if num % num2 == 0:
-#        return 1;
-#    else:
-#        num2 += 1;
-#        auxiliar = primo(num, num2);
-#        return auxiliar;
-
 comprobacion = 0;
 contador = 0;
+y = num;
 
-#Se debe comrpobar hasta que num y num2 sean iguales
-#Se puede intentar con un for.
-
-for x in range(0,num):
+for x in range(1,num):
+    y -=1 ;
     if num % num2 == 0:
-        print("He entrado en el if");
         if contador != 0:
             comprobacion = 1;
             contador += 1;
-            print("He entrado en el if del if");
-        else:
-            comprobacion = 0;
-            print("He entrado en el else del if");
+            break;
 
-    elif num == num2:
-        if contador == 0:
-            comprobacion = 0;
-        print("He entrado en el elif");
+        else:
+            if contador == 0:
+                if y == 1:
+                    comprobacion = 0;
+                    break;
+
+                else:
+                    comprobacion = 1;
+                    break;
+
+            else:
+                comprobacion = 1;
+                break;
 
     else:
-        num2 += 1;
-        print num2;
-        print("He entrado en el else");
+        if num == num2: 
+            if contador == 0:
+                comprobacion = 0;
+        
+        else:
+            num2 += 1;
 
-
-#comprobacion = primo(num,num2);
 
 if comprobacion == 0:
     print("Es primo");

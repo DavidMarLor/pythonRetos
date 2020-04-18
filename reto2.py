9dda46ac8a0d348975f9570b1fe9a430ad008783
David Martinez Lora <david@MacBook-Pro-de-David.local>
2020-04-18 13:41:22 +0200

    Resolucion con python 3.0

--- reto2.py
+++ reto2.py
@@ -1,31 +1,31 @@
 #Sacar la suma de los valores pares de la serie de fibonacci para una serie de elementos.
 
-print("Introduce un numero de elementos para la serie de Fibonacci");
-numElem = input();
+print("Introduce un numero de elementos para la serie de Fibonacci")
+numElem = input()
 
 
-print('Ha introducido: ', numElem);
+print('Ha introducido: ', numElem)
 
-auxiliar = 1;
-auxiliar2 = 1;
-total = 0;
+auxiliar = 1
+auxiliar2 = 1
+total = 0
 
-print auxiliar;
-print auxiliar2;
+print (auxiliar)
+print (auxiliar2)
 
 for x in range(0 , numElem):
     
-    cont = auxiliar + auxiliar2;
-    print cont;
+    cont = auxiliar + auxiliar2
+    print (cont)
 
     if cont % 2 == 0: 
-        total = cont + total;
+        total = cont + total
 
     #Intercambio de valores para realizar fibonacci
-    auxiliar2, auxiliar = auxiliar, auxiliar2;
-    auxiliar, cont = cont, auxiliar;
+    auxiliar2, auxiliar = auxiliar, auxiliar2
+    auxiliar, cont = cont, auxiliar
 
 
-print("El total de los pares es: ");
-print total;
+print("El total de los pares es: ")
+print (total)
 
